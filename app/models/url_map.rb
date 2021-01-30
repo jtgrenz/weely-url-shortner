11801b75c54acaf795a75b9d2c1f6bdf8f0ab7f1
JonG <2111778+jtgrenz@users.noreply.github.com>
2021-01-30 11:44:57 -0500

    use truncated salted sha for token
    initially thought it would be simpler and safer to use a string and
    increment it but you need to fetch the previous record's token first
    which can cause a race condition. Using a sha1 with a random salt and
    truncating should yeild sufficient randomnness and not be prone to
    collisions.

--- app/models/url_map.rb
+++ app/models/url_map.rb
@@ -2,6 +2,16 @@ class UrlMap < ApplicationRecord
   validates :url, :token, presence: true
   validates :url, :token, uniqueness: true
   validates :url, url: { no_local: true }
-  validates :token, format: { with: /\A[a-z]/ }
-  validates :token, length: { minimum: 3 }
+  validates :token, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :token, length: { minimum: 7 }
+
+  before_validation :generate_token
+
+  private
+
+  def generate_token
+    return token unless token.nil?
+    salted_url = "#{SecureRandom.hex(10)}#{url}"
+    self.token = Digest::SHA1.hexdigest(salted_url)[0..6]
+  end
 end
--- test/models/url_map_test.rb
+++ test/models/url_map_test.rb
@@ -2,31 +2,44 @@ require "test_helper"
 
 class UrlMapTest < ActiveSupport::TestCase
   test "url is a valid url" do
-    bad_url = UrlMap.new(url: "google.ca", token: "abc")
-    valid = UrlMap.new(url: "https://google.ca", token: "abc")
+    bad_url = UrlMap.new(url: "google.ca", token: "abcdef7")
+    valid = UrlMap.new(url: "https://google.ca", token: "abcdef7")
 
     assert valid.valid?
     refute bad_url.valid?
   end
 
   test "url and token are unique" do
-    UrlMap.create!(url: "https://google.ca", token: "abc")
-    duplicate = UrlMap.new(url: "https://google.ca", token: "abc")
+    UrlMap.create!(url: "https://google.ca", token: "abcdef7")
+    duplicate = UrlMap.new(url: "https://google.ca", token: "abcdef7")
     refute duplicate.valid?
   end
 
-  test "token is a minimum of 3 letters" do
-    url_map = UrlMap.new(url: "https://google.com", token: "a")
-    refute url_map.valid?
+  test "token is a minimum of 7 characters" do
+    bad_token = 'aB1'
+    valid_token = 'AbCdef7'
+
+    bad_url_map = UrlMap.new(url: "https://google.ca", token: bad_token)
+    valid_url_map = UrlMap.new(url: "https://google.ca", token: valid_token)
+
+    refute bad_url_map.token.length >= 7
+    refute bad_url_map.valid?
+
+    assert valid_url_map.token.length >= 7
+    assert valid_url_map.valid?
   end
 
-  test "token contains lowercase letters from a-z" do
-    upper_map = UrlMap.new(url: "https://google.com", token: "ABC")
-    number_map = UrlMap.new(url: "https://google.com", token: "123")
-    symbol_map = UrlMap.new(url: "https://google.com", token: "@%*")
+  test "token contains only alphanumeric characters" do
+    valid_token = "Ab9def7"
+    symbol_token = "&99d!f7"
 
-    refute upper_map.valid?
-    refute number_map.valid?
+    valid_map = UrlMap.new(url: "https://google.ca", token: valid_token)
+    symbol_map = UrlMap.new(url: "https://google.ca", token: symbol_token)
+
+    assert valid_map.valid?
+    assert valid_map.token.match(/\A[a-zA-Z0-9]+\z/)
+
     refute symbol_map.valid?
+    refute symbol_map.token.match(/\A[a-zA-Z0-9]+\z/)
   end
 end
